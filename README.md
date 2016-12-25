50bffcfea5447337adb5757faed3bc0c05ef4913
abinnz <aabingo1314@gmail.com>
2016-12-25 22:35:36 +0800

    创建新的分支

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 
 #### 1、restore one picture
 🌰  python alipayar.py input/IMG_1838.PNG
+🌰  python alipayar.py png
 #### 2、restore a lot of pictures
 🌰  python alipayar.py input/ output/
 
@@ -14,12 +15,44 @@ Python Imaging Library(PIL)
 
 maybe you should to add some paramether to suit your screenshot
 
-🌰  📱iPhone6
+🌰 📱iPhone6
+```
+[system]
+phone=iphone6
+```
+
+🌰 📱honor6
+```
+[system]
+phone=honor6
+```
 
 ```
-position = {
-    '750*1335':{'left':205,'top':638,'width':340} # 对应iPhone6的截图图片的 坐标点
-}
+[honor6]
+# 不同机型对应不同的width*height
+left=220
+top=618
+width=280
+height=280
+# 高度偏移值
+margin=7,4
+# 黑线宽度
+blanLineHeight=6
+# 黑线数量 固定值一般不需要改动
+blanLineSize=27
+
+[iphone6]
+# 不同机型对应不同的width*height
+left=205
+top=638
+width=340
+height=340
+# 高度偏移值
+margin=10,5
+# 黑线宽度
+blanLineHeight=7
+# 黑线数量 固定值一般不需要改动
+blanLineSize=27
 ```
 
 ![](https://ww4.sinaimg.cn/large/006tNc79jw1fb1zhrbookj30ja0y2dke.jpg)
--- alipayar.py
+++ alipayar.py
@@ -2,10 +2,10 @@
 
 from PIL import Image, ImageFilter
 import os,sys
+import configparser
 
-position = {
-    '750*1335':{'left':205,'top':638,'width':340} # 对应iPhone6的截图图片的 坐标点
-}
+# 线索图参数
+config = {}
 
 def handlerImg(path,savepath=''):
 
@@ -14,59 +14,108 @@ def handlerImg(path,savepath=''):
 
     #读取图像
     im = Image.open(path)
-    w=im.width
-    h=im.height
+    w = im.width
+    h = im.height
 
-    global position
-    pos=position.get(str(w)+'*'+str(h))
+    global config
+    pos=config.get(str(w)+'*'+str(h))
     if pos==None:
-        pos=position.get('750*1335')
+        pos=config.get('width*height')
 
     left=pos['left']
     top=pos['top']
     width=pos['width']
+    height=pos['height']
     #从截图中裁出线索图片
-    pic=im.crop((left,top,left+width,top+width))
+    pic=im.crop((left,top,left+width,top+height))
 
     # 高度偏移值
-    # 这些规则完全是从图片中找出来的，不一定完全正确，凭感觉，只是为了提高准确率
-    exc=2;
-    for index in range(0,27):
-        if index%6==0:
-            exc=exc+1;
-        copyandpaste(pic,exc+index*12,width)
+    margin = config.get('margin')
+    # 黑线宽度
+    blanLineHeight = config.get('blanLineHeight')
 
+    # 为了减少误差将线索图片分成两次循环
+    # 1 至 14条黑线
+    for index in range(0,int(config.get('blanLineSize')/2) + 1):
+        if index == 0:
+            copyandpaste(pic,margin[0],width,blanLineHeight)
+        else:
+            copyandpaste(pic,margin[0]+(margin[1]+blanLineHeight)*index,width,blanLineHeight)
+    # 27 至 15条黑线
+    for index in range(config.get('blanLineSize') - 1,int(config.get('blanLineSize') / 2) , -1):
+    	seq = config.get('blanLineSize') - 1 - index 
+    	if seq == 0:
+    		copyandpaste(pic,pic.height - margin[0] - blanLineHeight,width,blanLineHeight)
+    	else:
+    		copyandpaste(pic,pic.height - margin[0] - blanLineHeight - (blanLineHeight + margin[1])*seq,width,blanLineHeight)
     if ''==savepath:
         pic.show()
     else:
         pic.save(savepath+'/'+filename,extension)
 
-def copyandpaste(pic,start,width):
-    destpos=7
-    hight=3
-    tmp=pic.crop((0,start+(destpos-hight),width,start+destpos))
-    pic.paste(tmp,(0,start+destpos,width,start+destpos+hight))
-    tmp=pic.crop((0,start+destpos+destpos,width,start+destpos+destpos+destpos-hight))
-    pic.paste(tmp,(0,start+destpos+hight,width,start+destpos+destpos))
+def copyandpaste(pic,start,width,blanLineHeight):
+    halfHeight = int(blanLineHeight / 2)
+    # 方法一
+    # tmp=pic.crop((0,start - halfHeight,width,start))
+    # pic.paste(tmp,(0,start))
+    # tmp=pic.crop((0,start + blanLineHeight,width,start + blanLineHeight + halfHeight))
+    # pic.paste(tmp,(0,start + halfHeight))
+
+    # 方法二
+    tmp=pic.crop((0,start - 1,width,start))
+    for i in range(0,halfHeight):
+    	pic.paste(tmp,(0,start+i))
+    tmp=pic.crop((0,start + blanLineHeight,width,start + blanLineHeight + halfHeight))
+    for i in range(0,halfHeight):
+    	pic.paste(tmp,(0,start + halfHeight + i))
 
 def input():
+
     if (len(sys.argv))<2:
-        print '错误：请输入图片路径\npython alipayar.py [filepath]'
-        return;
+        print('错误：请输入图片路径\npython alipayar.py [filepath]')
+        return
     else:
-        path1=sys.argv[1] # 原路径
-        if os.path.isdir(path1):
-            if len(sys.argv)<3:
-                print '错误：请输入两个目录\npython alipayar.py [inputpath] [outputpath]'
-                return
-            else:
-                path2=sys.argv[2] # 输入目录
-                for file in os.listdir(path1):
-                    path = os.path.join(path1, file).lower()
-                    if path.endswith('png'):
-                        handlerImg(path,path2)
-        else:
-            handlerImg(sys.argv[1])
+    	if sys.argv[1] == 'png': # 新增png参数
+    		for file in os.listdir("."):
+    			if file.endswith('png') or file.endswith('PNG'): 
+    				handlerImg(file)
+    	else:
+	        path1=sys.argv[1] # 原路径
+	        if os.path.isdir(path1):
+	            if len(sys.argv)<3:
+	                print('错误：请输入两个目录\npython alipayar.py [inputpath] [outputpath]')
+	                return
+	            else:
+	                path2=sys.argv[2] # 输入目录
+	                for file in os.listdir(path1):
+	                    path = os.path.join(path1, file).lower()
+	                    if path.endswith('png'):
+	                        handlerImg(path,path2)
+	        else:
+	            handlerImg(sys.argv[1])
+
+def initConfig():
+	global config
+	appConfig = configparser.ConfigParser()
+	appConfig.read('alipayar_config.ini',encoding='utf-8')
+	phone = appConfig.get('system','phone')
+	left = int(appConfig.get(phone,'left'))
+	top = int(appConfig.get(phone,'top'))
+	width = int(appConfig.get(phone,'width'))
+	height = int(appConfig.get(phone,'height'))
+	marginArray = appConfig.get(phone,'margin').split(',')
+	margin = (int(marginArray[0]),int(marginArray[1]))
+	blanLineHeight = int(appConfig.get(phone,'blankLineHeight'))
+	blanLineSize = int(appConfig.get(phone,'blankLineSize'))
+	config['width*height'] = {'left': left,'top': top,'width': width,'height': height}
+	config['margin'] = margin
+	config['blanLineHeight'] = blanLineHeight
+	config['blanLineSize'] = blanLineSize
 
 if __name__ == '__main__':
+    initConfig()
     input()
+    
+
+
+    
